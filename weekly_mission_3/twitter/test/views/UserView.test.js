a158f540f18c7d6bcb3e9e60d966fd3de89df3ad
AntGtz <tranetrose@gmail.com>
2022-04-27 23:22:48 -0500

    requerimiento1

--- weekly_mission_3/twitter/test/views/UserView.test.js
+++ weekly_mission_3/twitter/test/views/UserView.test.js
@@ -1,10 +1,9 @@
 const UserView = require('./../../app/views/UserView')
 
 describe("Tests for Userview", () =>{
-    TextDecoderStream("1) return an error object when try to create a new user with a null payload", () => {
+    test("1) return an error object when try to create a new user with a null payload", () => {
         const payload = null
         const result = UserView.createUser(payload)
-        // 
         expect(result.error).toMatch(/payload no existe/)
     })
 })
