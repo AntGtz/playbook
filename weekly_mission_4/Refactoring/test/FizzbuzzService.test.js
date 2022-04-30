dd6e8d5b1bee511d2a69c5689df21ffed9e45ec8
AntGtz <tranetrose@gmail.com>
2022-04-29 22:30:23 -0500

    pruebas fizzbuzz

--- weekly_mission_4/Refactoring/test/FizzbuzzService.test.js
+++ weekly_mission_4/Refactoring/test/FizzbuzzService.test.js
@@ -1,4 +1,13 @@
 describe("Unidad de pruebas para FizzbuzzService", () => {
-    test("1)", () => {
+    test("1)Agregar trick a explorer FIZZBUZZ", () => {
+        const FizzbuzzService = require("./../app/lib/services/FizzbuzzService")
+        const explorer1 = {name: "Explorer1", score: 1}
+        expect(FizzbuzzService.applyValidationInExplorer(explorer1)).toStrictEqual({name: "Explorer1", score: 1, trick: 1})
+        const explorer3 = {name: "Explorer3", score: 3}
+        expect(FizzbuzzService.applyValidationInExplorer(explorer3)).toStrictEqual({name: "Explorer3", score: 3, trick: "FIZZ"})
+        const explorer5 = {name: "Explorer5", score: 5}
+        expect(FizzbuzzService.applyValidationInExplorer(explorer5)).toStrictEqual({name: "Explorer5", score: 5, trick: "BUZZ"})
+        const explorer15 = {name: "Explorer15", score: 15}
+        expect(FizzbuzzService.applyValidationInExplorer(explorer15)).toStrictEqual({name: "Explorer15", score: 15, trick: "FIZZBUZZ"})
     })
 })
